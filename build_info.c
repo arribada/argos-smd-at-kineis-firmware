b903f17d148f32b5dfbd80693a41f94e37763f57
Archie Atkinson <hello@archieatkinson.com>
2025-12-19 16:18:28 +0000

    Adds devcontainer

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "1d2c415*_DVTxStdb_gui_blind_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Oct 15 2025_13:32:57";
+const char uc_fw_vers_commit_id[] = "96ddb79*_DTxShtdwn_gui_basic_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Dec 19 2025_16:11:22";
