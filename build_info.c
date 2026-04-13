a7d973908a6fe78e82a9fc905d1a0848d3148dc7
gfo974 <geoffrey@arribada.org>
2026-04-13 17:57:14 +0400

    Fix GUI blocked mode when error occured

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "c24bd40*_DVTx_gui_basic_Pa_Spi,Apr 13 2026_16:28:51";
+const char uc_fw_vers_commit_id[] = "6a07a71*_Tx_gui_basic_Pa_Spi,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Apr 13 2026_17:44:29";
