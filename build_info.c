6a07a71713004ab3a577e53d2f631c3891ed8b79
gfo974 <geoffrey@arribada.org>
2026-04-13 16:29:23 +0400

    change spi pin to HIGH FREQ

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "c9fb83a*_VTx_gui_basic_Pa_Spi,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Apr 10 2026_15:31:51";
+const char uc_fw_vers_commit_id[] = "c24bd40*_DVTx_gui_basic_Pa_Spi,Apr 13 2026_16:28:51";
