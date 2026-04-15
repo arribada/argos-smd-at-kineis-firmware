6930ca39fb948b6c5821a9638b068d99c6fdcd3c
gfo974 <geoffrey@arribada.org>
2026-04-15 09:44:34 +0400

    fix polling dfu bootloader to accept 2MHz upload

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "6a07a71*_Tx_gui_basic_Pa_Spi,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Apr 13 2026_17:44:29";
+const char uc_fw_vers_commit_id[] = "a179a11*_Tx_gui_basic_Pa_Spi,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Apr 15 2026_09:40:06";
