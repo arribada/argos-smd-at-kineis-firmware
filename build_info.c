f777075f47737cac52f1a8c6173c01f7b77c4002
gfo974 <geoffrey@arribada.org>
2026-04-30 10:42:07 +0400

    Unified message type and modulation

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "4f3e265*_DVTx_uw_doppler_basic_StdAlone_Uart,Apr 30 2026_09:38:18";
+const char uc_fw_vers_commit_id[] = "a859aad*_DVTx_uw_doppler_basic_StdAlone_Uart,Apr 30 2026_10:39:24";
